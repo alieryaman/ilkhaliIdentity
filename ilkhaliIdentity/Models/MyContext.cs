f27f6735ced7d428cd7d03d4ecc92ab0a6dfe7ae
besir aydemir <besiraydemir@outlook.com>
2021-02-06 22:02:32 +0300

    Add project files.

---
+++ ilkhaliIdentity/Models/MyContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ilkhaliIdentity.Models
+{
+    public class MyContext: IdentityDbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+
+
+        
+
+
+    }
+}
